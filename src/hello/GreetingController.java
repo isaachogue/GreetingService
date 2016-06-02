a1190c659434e19dbc4302da38e15dc2728d162f
Isaac Hogue <isaac.hogue@activehogue.com>
2016-06-02 13:44:14 -0400

    adding a path for region
    testing pull from jenkins when github change is made

--- src/hello/GreetingController.java
+++ src/hello/GreetingController.java
@@ -1,8 +1,11 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,9 +13,16 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping("/region/greeting")
+    public Greeting greeting() {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                            String.format(template, new Region().getContent()));
     }
+
+    @RequestMapping("/region/{region}/greeting")
+    public Greeting greeting(@PathVariable("region") String region, HttpServletRequest request) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, region));
+    }
+    
 }
---
+++ src/hello/Region.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Region {
+	private String _region;
+	
+	public Region(){
+		_region = "USA";
+	}
+	
+	public String getContent(){
+		return _region;
+	}
+}
